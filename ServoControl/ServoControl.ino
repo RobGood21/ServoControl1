51e59e623b275ad02bdfcb41b461ac3ecbc0a27d
Rob Antonisse <robantonisse@gmail.com>
2019-04-30 15:59:54 +0200

    Add project files.

---
+++ ServoControl/ServoControl.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ServoControl.ino
+ Created:	4/30/2019 3:59:50 PM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
