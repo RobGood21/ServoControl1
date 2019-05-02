f753929d00b8f658671c87b5380319454bc84789
Rob Antonisse <robantonisse@gmail.com>
2019-05-02 11:54:50 +0200

    Servo

--- ServoControl/ServoControl.ino
+++ ServoControl/ServoControl.ino
@@ -4,15 +4,30 @@
  Author:	gebruiker
 */
 
-//declarations
+
+//Declaraties deKoder
+volatile unsigned long DEK_Tperiode; //laatst gemeten tijd 
+volatile unsigned int DEK_duur; //gemeten duur van periode tussen twee interupts
+boolean DEK_Monitor = false; //shows DCC commands as bytes
+byte DEK_Reg; //register voor de decoder 
+byte DEK_Status = 0;
+byte DEK_byteRX[6]; //max length commandoos for this decoder 6 bytes (5x data 1x error check)
+byte DEK_countPA = 0; //counter for preample
+byte DEK_BufReg[12]; //registerbyte for 12 command buffers //bit7= free (false) bit0= CV(true)
+byte DEK_Buf0[12];
+byte DEK_Buf1[12];
+byte DEK_Buf2[12];
+byte DEK_Buf3[12];
+byte DEK_Buf4[12];
+byte DEK_Buf5[12];
+
+//declarations shiftregisters
 byte SH_byte[2]; //2 bytes to be shifted out
 byte SH_bitcount;
 byte SH_bytecount;
 byte SH_fase;
 
 unsigned long CLK_time;
-
-
 void setup() {
 	DDRD |= (1 << 7); //pin7 OE (output enabled) van de shifts
 	Serial.begin(9600);
@@ -22,6 +37,370 @@ void setup() {
 	DDRD |= (1 << 6); //pin6 shift clock set as output
 
 	//PORTD |= (1 << 7); //set disable output	
+
+		//DeKoder part, interrupt on PIN2
+	DDRD &= ~(1 << 2);//bitClear(DDRD, 2); //pin2 input
+	DEK_Tperiode = micros();
+	EICRA |= (1 << 0);//EICRA – External Interrupt Control Register A bit0 > 1 en bit1 > 0 (any change)
+	EICRA &= ~(1 << 1);	//bitClear(EICRA, 1);
+	EIMSK |= (1 << INT0);//bitSet(EIMSK, INT0);//EIMSK – External Interrupt Mask Register bit0 INT0 > 1
+
+}
+ISR(INT0_vect) { //syntax voor een ISR
+//isr van PIN2
+	//DEK_Reg fase van bit ontvangst
+	//bit 0= bitpart ok (1) of failed(0)
+	//bit1= truepart 
+	//bit2=falsepart
+	//bit3= received bit true =true false =false
+	//bit4=restart, begin, failed as true
+
+	cli();
+	DEK_duur = (micros() - DEK_Tperiode);
+	DEK_Tperiode = micros();
+	if (DEK_duur > 50) {
+		if (DEK_duur < 62) {
+			DEK_Reg |= (1 << 0); //bitSet(DekReg, 0);
+
+			if (bitRead(DEK_Reg, 1) == false) {
+				DEK_Reg &= ~(1 << 2); //bitClear(DekReg, 2);
+				DEK_Reg |= (1 << 1);
+			}
+			else { //received full true bit
+
+				DEK_Reg |= (1 << 3);
+				DEK_BitRX();
+				DEK_Reg &= ~(1 << 2);//bitClear(DekReg, 2);
+				DEK_Reg &= ~(1 << 1); //bitClear(DekReg, 1);
+			}
+		}
+		else {
+			if (DEK_duur > 106) {
+
+				if (DEK_duur < 124) { //preferred 118 6us extra space in false bit
+					DEK_Reg |= (1 << 0); //bitSet(DekReg, 0);
+					if (bitRead(DEK_Reg, 2) == false) {
+						DEK_Reg &= ~(1 << 1); //bitClear(DekReg, 1);
+						DEK_Reg |= (1 << 2);  //bitSet(DekReg, 2);
+					}
+					else { //received full false bit
+						DEK_Reg &= ~(1 << 3); //bitClear(DekReg, 3);
+						DEK_BitRX();
+						DEK_Reg &= ~(1 << 2);//bitClear(DekReg, 2);
+						DEK_Reg &= ~(1 << 1); //bitClear(DekReg, 1);
+					}
+				}
+			}
+		}
+	}
+	sei();
+}
+void DEK_begin() {//runs when bit is corrupted, or command not correct
+	//lesscount++;
+	DEK_countPA = 0;
+	DEK_Reg = 0;
+	DEK_Status = 0;
+	for (int i = 0; i < 6; i++) {
+		DEK_byteRX[i] = 0; //reset receive array
+	}
+}
+void DEK_BufCom(boolean CV) { //create command in Buffer
+	byte i = 0;
+	while (i < 12) {
+
+		if (bitRead(DEK_BufReg[i], 7) == false) {
+			DEK_BufReg[i] = 0; //clear found buffer
+
+
+			DEK_Buf0[i] = DEK_byteRX[0];
+			DEK_Buf1[i] = DEK_byteRX[1];
+			DEK_Buf2[i] = DEK_byteRX[2];
+
+			if (CV == true) {
+				DEK_BufReg[i] |= (1 << 0); //set for CV
+				DEK_Buf3[i] = DEK_byteRX[3];
+				DEK_Buf4[i] = DEK_byteRX[4];
+				DEK_Buf5[i] = DEK_byteRX[5];
+			}
+			else {
+
+				DEK_Buf3[i] = 0;
+				DEK_Buf4[i] = 0;
+				DEK_Buf5[i] = 0;
+			}
+			DEK_BufReg[i] |= (1 << 7); //claim buffer
+			i = 15;
+		}
+		i++;
+	} //close for loop
+} //close void
+void DEK_BitRX() { //new version
+	static byte countbit = 0; //counter received bits
+	static byte countbyte = 0;
+	static byte n = 0;
+	DEK_Reg |= (1 << 4);//resets and starts process if not reset in this void
+	switch (DEK_Status) {
+		//*****************************
+	case 0: //Waiting for preample 
+		if (bitRead(DEK_Reg, 3) == true) {
+			DEK_countPA++;
+			if (DEK_countPA > 12) {
+				DEK_Status = 1;
+				countbit = 0;
+				countbyte = 0;
+			}
+			bitClear(DEK_Reg, 4);
+		}
+		break;
+		//*************************
+	case 1: //Waiting for false startbit
+		if (bitRead(DEK_Reg, 3) == false) { //startbit receive
+			DEK_countPA = 0;
+			DEK_Status = 2;
+		}
+		//if Dekreg bit 3= true no action needed.
+		bitClear(DEK_Reg, 4); //correct, so resume process
+		break;
+		//*************************
+	case 2: //receiving data
+		if (bitRead(DEK_Reg, 3) == true) DEK_byteRX[countbyte] |= (1 << (7 - countbit));
+		countbit++;
+		if (countbit == 8) {
+			countbit = 0;
+			DEK_Status = 3;
+			countbyte++;
+		}
+		bitClear(DEK_Reg, 4); //correct, so resume process
+		break;
+		//*************************
+	case 3: //waiting for separating or end bit
+		if (bitRead(DEK_Reg, 3) == false) { //false bit
+			DEK_Status = 2; //next byte
+			if ((bitRead(DEK_byteRX[0], 6) == false) & (bitRead(DEK_byteRX[0], 7) == true))bitClear(DEK_Reg, 4); //correct, so resume process	
+		}
+		else { //true bit, end bit, only 3 byte and 6 byte commands handled by this dekoder
+			switch (countbyte) {
+			case 3: //Basic Accessory Decoder Packet received
+				//check error byte
+				if (DEK_byteRX[2] = DEK_byteRX[0] ^ DEK_byteRX[1])DEK_BufCom(false);
+				break; //6
+			case 6: ///Accessory decoder configuration variable Access Instruction received (CV)
+				//in case of CV, handle only write command
+				if (bitRead(DEK_byteRX[2], 3) == true && (bitRead(DEK_byteRX[2], 2) == true)) {
+					//check errorbyte and make command
+					if (DEK_byteRX[5] = DEK_byteRX[0] ^ DEK_byteRX[1] ^ DEK_byteRX[2] ^ DEK_byteRX[3] ^ DEK_byteRX[4])DEK_BufCom(true);
+				}
+				break;
+			} //close switch bytecount
+		}//close bittype
+		break;
+		//***************************************
+	} //switch dekstatus
+	if (bitRead(DEK_Reg, 4) == true)DEK_begin();
+}
+void DEK_DCCh() { //handles incoming DCC commands, called from loop()
+	static byte n = 0; //one buffer each passing
+	byte temp;
+	int decoder;
+	int channel = 1;
+	int adres;
+	boolean port = false;
+	boolean onoff = false;
+	int cv;
+	int value;
+
+	//translate command
+	if (bitRead(DEK_BufReg[n], 7) == true) {
+		decoder = DEK_Buf0[n] - 128;
+		if (bitRead(DEK_Buf1[n], 6) == false)decoder = decoder + 256;
+		if (bitRead(DEK_Buf1[n], 5) == false)decoder = decoder + 128;
+		if (bitRead(DEK_Buf1[n], 4) == false)decoder = decoder + 64;
+		//channel
+		if (bitRead(DEK_Buf1[n], 1) == true) channel = channel + 1;
+		if (bitRead(DEK_Buf1[n], 2) == true) channel = channel + 2;
+		//port
+		if (bitRead(DEK_Buf1[n], 0) == true)port = true;
+		//onoff
+		if (bitRead(DEK_Buf1[n], 3) == true)onoff = true;
+		//CV
+		if (bitRead(DEK_BufReg[n], 0) == true) {
+			cv = DEK_Buf3[n];
+			if (bitRead(DEK_Buf2[n], 0) == true)cv = cv + 256;
+			if (bitRead(DEK_Buf2[n], 1) == true)cv = cv + 512;
+			cv++;
+			value = DEK_Buf4[n];
+		}
+		else {
+			cv = 0;
+			value = 0;
+		}
+		COM_exe(bitRead(DEK_BufReg[n], 0), decoder, channel, port, onoff, cv, value);
+
+
+		//Show Monitor (bytes)
+		if (DEK_Monitor == true) {
+			Serial.print("buffer= ");
+			Serial.print(n);
+			Serial.print("  value:  ");
+			Serial.print(bitRead(DEK_BufReg[n], 7));
+			Serial.print(bitRead(DEK_BufReg[n], 6));
+			Serial.print(bitRead(DEK_BufReg[n], 5));
+			Serial.print(bitRead(DEK_BufReg[n], 4));
+			Serial.print(bitRead(DEK_BufReg[n], 3));
+			Serial.print(bitRead(DEK_BufReg[n], 2));
+			Serial.print(bitRead(DEK_BufReg[n], 1));
+			Serial.print(bitRead(DEK_BufReg[n], 0));
+			Serial.println("");
+
+			temp = DEK_Buf0[n];
+			Serial.print(bitRead(temp, 7));
+			Serial.print(bitRead(temp, 6));
+			Serial.print(bitRead(temp, 5));
+			Serial.print(bitRead(temp, 4));
+			Serial.print(bitRead(temp, 3));
+			Serial.print(bitRead(temp, 2));
+			Serial.print(bitRead(temp, 1));
+			Serial.print(bitRead(temp, 0));
+			Serial.println("");
+
+			temp = DEK_Buf1[n];
+			Serial.print(bitRead(temp, 7));
+			Serial.print(bitRead(temp, 6));
+			Serial.print(bitRead(temp, 5));
+			Serial.print(bitRead(temp, 4));
+			Serial.print(bitRead(temp, 3));
+			Serial.print(bitRead(temp, 2));
+			Serial.print(bitRead(temp, 1));
+			Serial.print(bitRead(temp, 0));
+			Serial.println("");
+
+			temp = DEK_Buf2[n];
+			Serial.print(bitRead(temp, 7));
+			Serial.print(bitRead(temp, 6));
+			Serial.print(bitRead(temp, 5));
+			Serial.print(bitRead(temp, 4));
+			Serial.print(bitRead(temp, 3));
+			Serial.print(bitRead(temp, 2));
+			Serial.print(bitRead(temp, 1));
+			Serial.print(bitRead(temp, 0));
+			Serial.println("");
+
+			temp = DEK_Buf3[n];
+			Serial.print(bitRead(temp, 7));
+			Serial.print(bitRead(temp, 6));
+			Serial.print(bitRead(temp, 5));
+			Serial.print(bitRead(temp, 4));
+			Serial.print(bitRead(temp, 3));
+			Serial.print(bitRead(temp, 2));
+			Serial.print(bitRead(temp, 1));
+			Serial.print(bitRead(temp, 0));
+			Serial.println("");
+
+			temp = DEK_Buf4[n];
+			Serial.print(bitRead(temp, 7));
+			Serial.print(bitRead(temp, 6));
+			Serial.print(bitRead(temp, 5));
+			Serial.print(bitRead(temp, 4));
+			Serial.print(bitRead(temp, 3));
+			Serial.print(bitRead(temp, 2));
+			Serial.print(bitRead(temp, 1));
+			Serial.print(bitRead(temp, 0));
+			Serial.println("");
+
+
+			temp = DEK_Buf5[n];
+			Serial.print(bitRead(temp, 7));
+			Serial.print(bitRead(temp, 6));
+			Serial.print(bitRead(temp, 5));
+			Serial.print(bitRead(temp, 4));
+			Serial.print(bitRead(temp, 3));
+			Serial.print(bitRead(temp, 2));
+			Serial.print(bitRead(temp, 1));
+			Serial.print(bitRead(temp, 0));
+			Serial.println("");
+			Serial.println("------");
+
+		}
+		//clear buffer
+		DEK_BufReg[n] = 0;
+		DEK_Buf0[n] = 0;
+		DEK_Buf1[n] = 0;
+		DEK_Buf2[n] = 0;
+		DEK_Buf3[n] = 0;
+		DEK_Buf4[n] = 0;
+		DEK_Buf5[n] = 0;
+	}
+	n++;
+	if (n > 12)n = 0;
+}
+void COM_exe(boolean type, int decoder, int channel, boolean port, boolean onoff, int cv, int value) {
+	//type=CV(true) or switch(false)
+	//decoder basic adres of decoder 
+	//channel assigned one of the 4 channels of the decoder (1-4)
+	//Port which port R or L
+	//onoff bit3 port on or port off
+	//cv cvnumber
+	//cv value
+	int adres;
+	adres = ((decoder - 1) * 4) + channel;
+	//Applications 
+	//APP_Monitor(type, adres, decoder, channel, port, onoff, cv, value);
+	APP_function(type, adres, decoder, channel, port, onoff, cv, value);
+}
+void APP_Monitor(boolean type, int adres, int decoder, int channel, boolean port, boolean onoff, int cv, int value) {
+	//application for DCC monitor
+	if (type == true) {
+		Serial.print("CV:   , ");
+	}
+	else {
+		Serial.print("Switch, ");
+	}
+	Serial.print("Adres: ");
+	Serial.print(adres);
+	Serial.print("(");
+	Serial.print(decoder);
+	Serial.print("-");
+	Serial.print(channel);
+	Serial.print("), ");
+	//cv
+	if (type == true) {
+		Serial.print("CV: ");
+		Serial.print(cv);
+		Serial.print(", waarde: ");
+		Serial.print(value);
+	}
+	else {
+		if (port == true) {
+			Serial.print("A<, ");
+		}
+		else {
+			Serial.print("R>, ");
+		}
+		if (onoff == true) {
+			Serial.print("On.");
+		}
+		else {
+			Serial.print("Off");
+		}
+	}
+	Serial.println("");
+}
+void APP_function(boolean type, int adres, int decoder, int channel, boolean port, boolean onoff, int cv, int value) {
+	//executes DCC commands called from COM_exe
+	if (type == false) { //switch command
+		if (adres > 0 & adres < 9) {
+			adres--;
+			if (port == true) {
+				SH_byte[0] |= (1 << adres);
+			}
+			else {
+				SH_byte[0] &= ~(1 << adres);
+			}
+		}
+	}
+	else { //CV command
+
+	}
 }
 
 void SHIFT() {
@@ -68,7 +447,6 @@ void SHIFT() {
 		break;
 	}
 }
-
 void CLK_exe() {//called from loop
 	//Serial.println(SH_byte[0] + SH_byte[1]);	
 
@@ -85,17 +463,17 @@ void CLK_exe() {//called from loop
 			SH_byte[0] = SH_byte[0] << 1;
 		}
 	}
-if (SH_byte[0] + SH_byte[1] == 0) 	SH_byte[0] = 1;
+	if (SH_byte[0] + SH_byte[1] == 0) 	SH_byte[0] = 1;
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+	DEK_DCCh();
 	SHIFT();
 
 	//slowtimer
 	if (millis() - CLK_time > 500) {
-		CLK_exe();
+		//CLK_exe();
 		CLK_time = millis();
 	}
-
 }
